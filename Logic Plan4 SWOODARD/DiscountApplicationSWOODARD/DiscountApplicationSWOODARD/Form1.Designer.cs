ce1fbfa65b59a48484a6acfb6c7c01d8c788935a
Steven Woodard <38500060+swood049@users.noreply.github.com>
2019-11-16 12:29:46 -0500

    Upload to github

---
+++ Logic Plan4 SWOODARD/DiscountApplicationSWOODARD/DiscountApplicationSWOODARD/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace DiscountApplicationSWOODARD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.discountRateLabel = new System.Windows.Forms.Label();
+            this.amountSavedLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Package(s) Purchased:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(205, 20);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Discount Rate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Amount Saved:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Total After Discount:";
+            // 
+            // discountRateLabel
+            // 
+            this.discountRateLabel.Location = new System.Drawing.Point(152, 61);
+            this.discountRateLabel.Name = "discountRateLabel";
+            this.discountRateLabel.Size = new System.Drawing.Size(99, 26);
+            this.discountRateLabel.TabIndex = 5;
+            this.discountRateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // amountSavedLabel
+            // 
+            this.amountSavedLabel.Location = new System.Drawing.Point(152, 87);
+            this.amountSavedLabel.Name = "amountSavedLabel";
+            this.amountSavedLabel.Size = new System.Drawing.Size(99, 26);
+            this.amountSavedLabel.TabIndex = 6;
+            this.amountSavedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.Location = new System.Drawing.Point(152, 120);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(99, 26);
+            this.totalLabel.TabIndex = 7;
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(349, 63);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 8;
+            this.calculateButton.Text = "Calculate";
+            this.toolTip1.SetToolTip(this.calculateButton, "Calculate The Discounted Price");
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(349, 109);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 307);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.amountSavedLabel);
+            this.Controls.Add(this.discountRateLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculate Software Sales SWOODARD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label discountRateLabel;
+        private System.Windows.Forms.Label amountSavedLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Logic Plan4 SWOODARD/DiscountApplicationSWOODARD/DiscountApplicationSWOODARD/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscountApplicationSWOODARD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //Declare variables: price, quantity, discRate, amtOfDisc, originalTotal, total
+            const decimal price = 99.00m;
+            int quantity;
+            decimal discRate;
+            decimal amtOfDisc;
+            decimal originalTotal;
+            decimal total;
+
+            //Parse, Obtain quantity from user
+            if (int.TryParse(numberTextBox.Text, out quantity))
+            {
+                //if quantity is less than or equal to 0
+                if(quantity <= 0)
+                {
+                    //prompt user to try again
+                    MessageBox.Show("Error, purchase has to be greater than zero. Try again.");
+                }
+                else
+                {//quantity is between 0 and 10
+                    if (quantity > 0 && quantity <= 9)
+                    {//Assign 0% to discRate
+                        discRate = 0.0m;
+                    }//quantity is between 9 and 20
+                    else if(quantity > 9 && quantity <= 19)
+                    {//Assign 0.20 to discRate
+                        discRate = 0.2m;
+                    }//quantity is between 19 and 50
+                    else if (quantity > 19 && quantity <= 49)
+                    {//Assign 0.30 to discRate
+                        discRate = 0.3m;
+                    }//quantity is between 49 and 100
+                    else if (quantity > 49 && quantity <= 99)
+                    {//Assign 0.40 to discRate
+                        discRate = 0.4m;
+                    }//quantity is 100 or above
+                    else
+                    {//Assign 0.50 to discRate
+                        discRate = 0.5m;
+                    }
+                    //calculate original total
+                    originalTotal = price * quantity;
+                    //calculate amount of discount
+                    amtOfDisc = originalTotal * discRate;
+                    //calculate total
+                    total = originalTotal - amtOfDisc;
+
+                    //output discount rate, amount total discount, and total
+                    discountRateLabel.Text = discRate.ToString("p");
+                    amountSavedLabel.Text = amtOfDisc.ToString("c");
+                    totalLabel.Text = total.ToString("c");
+
+                }
+            }
+            else
+            {//handle garbage
+                MessageBox.Show("please input an integer");
+            }
+            
+        }
+    }
+}
